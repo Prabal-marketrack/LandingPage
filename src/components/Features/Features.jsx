548481f299fe5f45ebf909a43bbc7334386d67f2
Prabal-marketrack <143197304+Prabal-marketrack@users.noreply.github.com>
2023-12-25 00:18:47 +0530

    Update Features.jsx

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -44,7 +44,7 @@ const Features = () => {
                     scrub: true,
                     start: "2% 90%",
                     end: "7% 60%",
-                    markers: false,
+                    markers: true,
                     invalidateOnRefresh: true,
                     onLeave: () => {
                         setLottiePlay(true);
